31568974d1adf9d3f104dd92375f97a3e7a09c9a
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-12-16 12:54:04 +0700

    login back

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,14 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-
-$routes->get('/', 'Home::index');
-$routes->get('/PenilaianPerusahaan', 'PenilaianPerusahaan::index');
-$routes->post('PenilaianPerusahaan/tambah', 'PenilaianPerusahaan::tambah');
-$routes->get('PenilaianPerusahaan/hapus/(:num)', 'PenilaianPerusahaan::hapus/$1');
-$routes->get('/Rekomendasi', 'Rekomendasi::index');
-$routes->post('/Rekomendasi/cariRekomendasi', 'Rekomendasi::cariRekomendasi');
-$routes->get('PenilaianPelanggan', 'PenilaianPelanggan::index');
-$routes->get('/form', 'PenilaianPelanggan::form');
-$routes->post('/PenilaianPelanggan/save', 'PenilaianPelanggan::save');
-$routes->get('/PenilaianPelanggan/(:num)', 'PenilaianPelanggan::detail/$1');
+$routes->group('', ['filter' => 'login'], function ($routes) {
+   $routes->get('/', 'Home::index');
+   $routes->get('/PenilaianPerusahaan', 'PenilaianPerusahaan::index');
+   $routes->post('PenilaianPerusahaan/tambah', 'PenilaianPerusahaan::tambah');
+   $routes->get('PenilaianPerusahaan/hapus/(:num)', 'PenilaianPerusahaan::hapus/$1');
+   $routes->get('/Rekomendasi', 'Rekomendasi::index');
+   $routes->post('/Rekomendasi/cariRekomendasi', 'Rekomendasi::cariRekomendasi');
+   $routes->get('PenilaianPelanggan', 'PenilaianPelanggan::index');
+   $routes->get('/form', 'PenilaianPelanggan::form');
+   $routes->post('/PenilaianPelanggan/save', 'PenilaianPelanggan::save');
+   $routes->get('/PenilaianPelanggan/(:num)', 'PenilaianPelanggan::detail/$1');
+});
