8b5d314cb2fc9f68b8a11a36e62ef5660c02e999
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-12-16 16:09:01 +0700

    refactor

--- app/Views/RekomendasiPelanggan/index.php
+++ app/Views/RekomendasiPelanggan/index.php
@@ -1,3 +1,6 @@
+<?= $this->extend('template/v_template'); ?>
+
+<?= $this->section('content') ?>
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800"><?= $judul ?></h1>
 
@@ -22,43 +25,45 @@
     </div>
 
     <div class="card">
-            <div class="table-responsive">
-                <table class='table'>
-                    <thead>
+        <div class="table-responsive">
+            <table class='table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Jenis Kayu</th>
+                        <th>Merek Kayu</th>
+                        <th>Tingkat Tekstur</th>
+                        <th>Tingkat Ketahanan</th>
+                        <th>Tingkat Keperawatan</th>
+                        <th>Harga</th>
+                        <th>Massa Jenis</th>
+                        <th>Tipe Finishing Warna</th>
+                        <th>Kelebihan</th>
+                        <th>Kekurangan</th>
+                        <th>Opsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($hasilPencarian ?? [] as $row) :  ?>
                         <tr>
-                            <th>Id</th>
-                            <th>Jenis Kayu</th>
-                            <th>Merek Kayu</th>
-                            <th>Tingkat Tekstur</th>
-                            <th>Tingkat Ketahanan</th>
-                            <th>Tingkat Keperawatan</th>
-                            <th>Harga</th>
-                            <th>Massa Jenis</th>
-                            <th>Tipe Finishing Warna</th>
-                            <th>Kelebihan</th>
-                            <th>Kekurangan</th>
-                            <th>Opsi</th>
+                            <td><?= $row['id']; ?></td>
+                            <td><?= $row['Jenis_Kayu']; ?></td>
+                            <td><?= $row['Merek_Kayu']; ?></td>
+                            <td><?= $row['Tingkat_Tekstur']; ?></td>
+                            <td><?= $row['Tingkat_Ketahanan']; ?></td>
+                            <td><?= $row['Tingkat_Keperawatan']; ?></td>
+                            <td><?= $row['Harga']; ?></td>
+                            <td><?= $row['Massa_Jenis']; ?></td>
+                            <td><?= $row['Tipe_Finishing_Warna']; ?></td>
+                            <td><?= $row['Kelebihan']; ?></td>
+                            <td><?= $row['Kekurangan']; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($hasilPencarian ?? [] as $row) :  ?>
-                            <tr>
-                                <td><?= $row['id']; ?></td>
-                                <td><?= $row['Jenis_Kayu']; ?></td>
-                                <td><?= $row['Merek_Kayu']; ?></td>
-                                <td><?= $row['Tingkat_Tekstur']; ?></td>
-                                <td><?= $row['Tingkat_Ketahanan']; ?></td>
-                                <td><?= $row['Tingkat_Keperawatan']; ?></td>
-                                <td><?= $row['Harga']; ?></td>
-                                <td><?= $row['Massa_Jenis']; ?></td>
-                                <td><?= $row['Tipe_Finishing_Warna']; ?></td>
-                                <td><?= $row['Kelebihan']; ?></td>
-                                <td><?= $row['Kekurangan']; ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
+</div>
+
+<?= $this->endSection() ?>
