140329055f2a0e7cdd696e3bac2c8cae7d6edc87
Gabrielpjt <gabrielpanjaitan46@gmail.com>
2023-12-18 16:31:02 +0700

    perbaiki

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -3,6 +3,10 @@
 
 <!-- Page Heading -->
 <h1 class="h3 mb-4 text-gray-800"><?= $judul; ?></h1>
+<h2 class="h3 mb-4 text-gray-800">Deskripsi Sistem</h1>
+
+<p class="h3 mb-4 text-gray-800">Toko material memilikan 4 fungsi sistem yang bekerja : menampilkan data penilaian perusahaan, menampilkan data penilaian pelanggan, membuat rekomendasi, dan menyediakan form penilaian pelanggan.</h1>
+
 
 </div>
 <!-- /.container-fluid -->
