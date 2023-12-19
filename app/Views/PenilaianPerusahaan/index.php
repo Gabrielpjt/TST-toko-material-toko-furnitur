c46c94ff4a27767aa007a34472dbd4f8eccafb70
rasyadanfz <18221103@std.stei.itb.ac.id>
2023-12-19 08:14:05 +0700

    update views

--- app/Views/PenilaianPerusahaan/index.php
+++ app/Views/PenilaianPerusahaan/index.php
@@ -1,3 +1,6 @@
+<?= $this->extend('template/v_template'); ?>
+
+<?= $this->section('content') ?>
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800"><?= $judul ?></h1>
@@ -192,5 +195,5 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
-
+<?= $this->endSection() ?>
 
